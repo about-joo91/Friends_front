451e0598652c3c0633a3c09f4132c791cf9242b6
khw7876 <skanahrwi@naver.com>
2022-07-04 00:09:21 +0900

    feature/Ko 테스트 댓글 불러오기, 로그인 한 계정의 댓글 수정, 삭제 버튼 구현

--- Kotest/comment_test/comment.js
+++ Kotest/comment_test/comment.js
@@ -6,7 +6,6 @@ function get_cookie(name) {
         const cookies = document.cookie.split(';');
         for (let i = 0; i < cookies.length; i++) {
             const cookie = cookies[i].trim();
-            // Does this cookie string begin with the name we want?
             if (cookie.substring(0, name.length + 1) === (name + '=')) {
                 cookie_value = decodeURIComponent(cookie.substring(name.length + 1));
                 break;
@@ -16,11 +15,22 @@ function get_cookie(name) {
     return cookie_value;
 }
 const csrftoken = get_cookie('csrftoken')
+// payload에서 user_id를 조회하기 위한 코드
+function parseJwt (token) {
+    var base64Url = localStorage.getItem("access").split('.')[1];
+    var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+    var jsonPayload = decodeURIComponent(window.atob(base64).split('').map(function(c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
+    }).join(''));
 
+    return JSON.parse(jsonPayload);
+};
 
+// 서버에서 get으로 댓글을 불러오는 코드
+const comment_list = document.querySelector('.db_comment_box')
 window.onload = async function get_commnet() {
 
-
+    
     const result = await fetch(BASE_URL + '/comment/1', {
         method: 'GET',
         mode: 'cors',
@@ -35,18 +45,39 @@ window.onload = async function get_commnet() {
     })
     let res = await result.json()
     if (result.status == 200){
+        let tmp_comment = ``
         for(let i = 0; i < res.length; i++){
             comment = res[i]
             console.log(comment)
+            if (parseJwt(localStorage.getItem("access")).user_id == comment.user){
+                tmp_comment += 
+            `<div class="db_comment">
+                <div class="db_comment_comment">
+                ${comment.author}님의 댓글 :<br> ${comment.comment} user_id = ${comment.user} 
+                </div>
+                <div class="db_comment_edit">
+                수정
+                </div>
+                <div class="db_comment_delete">
+                삭제
+                </div>
+            </div>`
+            }
+            else {
+            tmp_comment += 
+            `<div class="db_comment">
+                ${comment.author}님의 댓글 :<br> ${comment.comment} user_id = ${comment.user}
+            </div>`
+            }
         }
+        comment_list.innerHTML = tmp_comment
     }
     else {
         alert("댓글 작성이 실패했습니다!!")
     }
 }
 
-
-
+// 댓글을 작성하는 코드
 async function make_comment() {
     const comment = document.getElementById('new_comment').value;
 
