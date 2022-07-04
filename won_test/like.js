33ceac6015e026906e9a16cd788ac2a991a6df76
최희원 <choehuiwon@choehuiwon-ui-MacBookAir.local>
2022-07-04 13:52:55 +0900

    feat/좋아요 JWT토큰 넣어서 작동하도록 수정

--- won_test/like.js
+++ won_test/like.js
@@ -1,21 +1,46 @@
 const backend_base_url = "http://127.0.0.1:8000"
-const frontend_base_url = "http://127.0.0.1:5502"
+const frontend_base_url = "http://127.0.0.1:5500"
 
 let liked = false
 const urlParams = new URLSearchParams(window.location.pathname);
-// const post_id = urlParams.get('id'); //?
 const post_id = 1
+
+
+function get_cookie(name) {
+    let cookie_value = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookie_value = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookie_value;
+}
+const csrftoken = get_cookie('csrftoken')
+
 
 
 async function postLike(post_id){
     const response = await fetch(`${backend_base_url}/won_test/like/${post_id}`,{
         headers:{
-            'Authorization' : 'Bearer '+ localStorage.getItem("access")},
+            'Authorization' : 'Bearer '+ localStorage.getItem("access"),
+            "Access-Control-Allow-Origin": "*",
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrftoken},
+
         method:'POST',
+        mode: 'cors',
     })
 
     if (response.status ==200){
+        console.log(response)
         response_json = await response.json()
+        console.log(response_json)
         return response_json
     }else{
         alert(response.status)
@@ -29,6 +54,6 @@ async function like(){
     }else{
         like_button.classList.replace('fa-regular', 'fa-solid')
     }
-    const response = await postLike(post_id)
-    console.log(response, "좋아요")
+    const response = postLike(post_id)
+    console.log(response)
 }
--- won_test/mypage.js
+++ won_test/mypage.js
@@ -1,11 +1,12 @@
 const modalButton = document.querySelector(".modalButton");
 const modal = document.querySelector(".modal");
 const closeModal = document.querySelector(".closeModal");
-const modal__overlay = document.querySelector(".modal_overlay");
+const modal_overlay = document.querySelector(".modal_overlay");
 
 modalButton.addEventListener("click", () => {
     modal.classList.remove("hidden");
 });
-modal__overlay.addEventListener("click", () => {
+modal_overlay.addEventListener("click", () => {
     modal.classList.add("hidden");
 });
+
