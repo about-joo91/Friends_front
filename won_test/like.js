a55aa4813a86a4961b5f983f5ea9cc211ba9e731
최희원 <choehuiwon@choehuiwon-ui-MacBookAir.local>
2022-07-04 11:46:58 +0900

    feat/좋아요 js 수정

--- won_test/like.js
+++ won_test/like.js
@@ -1,41 +1,20 @@
 const backend_base_url = "http://127.0.0.1:8000"
-const frontend_base_url = "http://127.0.0.1:5501"
+const frontend_base_url = "http://127.0.0.1:5502"
 
 let liked = false
-const urlParams = new URLSearchParams(window.location.search);
+const urlParams = new URLSearchParams(window.location.pathname);
 // const post_id = urlParams.get('id'); //?
 const post_id = 1
 
-like_empty.addEventListener('click',()=>{
-    like_fill.style.display = 'block';
-    like_empty.style.display = 'none';
-    alert("좋아요 완료!")
-    postLike(post_id)
-})
 
-like_fill.addEventListener('click',()=>{
-    like_empty.style.display = 'block';
-    like_fill.style.display = 'none';
-    alert("좋아요 취소 완료!")
-    deleteLike(post_id)
-})
-
-
-async function postLike(post_id) {
-    console.log(post_id)
-    const response = await fetch(`${backend_base_url}/blog/like/${post_id}`, {
-        // headers:{
-        //     'Authorization':localStorage.getItem("refresh")}, // 모르겠음
-        method:"POST",
+async function postLike(post_id){
+    const response = await fetch(`${backend_base_url}/won_test/like/${post_id}`,{
         headers:{
-            Accept:"application/json",
-            'Content-type':'application/json'
-        },
-
+            'Authorization' : 'Bearer '+ localStorage.getItem("access")},
+        method:'POST',
     })
 
-    if (response.status == 200){
-        // window.location.replace(`${frontend_base_url}/login.html`);
+    if (response.status ==200){
         response_json = await response.json()
         return response_json
     }else{
@@ -43,42 +22,13 @@ async function postLike(post_id) {
     }
 }
 
-async function deleteLike(post_id) {
-    const response = await fetch(`${backend_base_url}/blog/like/${post_id}`, {
-        method:"DELETE",
-        
-    })
-    if (response.status == 200){
-        response_json = await response.json()
-        return response_json
+async function like(){
+    const like_button = document.getElementById("like_button")
+    if (like_button.classList.contains('fa-solid')){
+        like_button.classList.replace('fa-solid', 'fa-regular')
     }else{
-        alert(response.status)
+        like_button.classList.replace('fa-regular', 'fa-solid')
     }
+    const response = await postLike(post_id)
+    console.log(response, "좋아요")
 }
-
-async function getLike(article_id){
-    const response = await fetch(`${backend_base_url}/blog/like/${post_id}`,{
-        method:'GET',
-    })
-
-    if (response.status == 200){
-        response_json = await response.json()
-        console.log(response_json)
-        return response_json
-    }else{
-        alert(response.status)
-    }
-}
-
-
-async function updateLike(){
-    const response = await getLike(post_id)
-    console.log(response)
-    liked = response.liked
-    if(!liked){
-        like_empty.style.display = 'block';
-        like_fill.style.display = 'none';
-    }
-}
-
-updateLike();
