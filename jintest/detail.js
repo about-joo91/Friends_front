8db4cdd8b29b47ece81a7512de54d0b101d8995e
hajin <f-a-k@naver.com>
2022-07-05 10:03:12 +0900

    test/ content like test complate / get save_posts

--- jintest/detail.js
+++ jintest/detail.js
@@ -1,28 +1,79 @@
+const BASE_URL = 'http://127.0.0.1:8000';
+
+
+function get_cookie(name) {
+    let cookie_value = null;
+    if (document.cookie && document.cookie !== '') {
+        const cookies = document.cookie.split(';');
+        for (let i = 0; i < cookies.length; i++) {
+            const cookie = cookies[i].trim();
+            if (cookie.substring(0, name.length + 1) === (name + '=')) {
+                cookie_value = decodeURIComponent(cookie.substring(name.length + 1));
+                break;
+            }
+        }
+    }
+    return cookie_value;
+}
+const csrftoken = get_cookie('csrftoken')
+
+
+
+
+const post_id = "15/"
+
 let bookmark_empty = document.getElementById("empty_bookmark")
 let bookmark_fill = document.getElementById("fill_bookmark")
-let like_empty = document.getElementById("empty_like")
-let like_fill = document.getElementById("fill_like")
 
-bookmark_empty.addEventListener('click',()=>{
-    bookmark_fill.style.display = 'block';
-    bookmark_empty.style.display = 'none';
-    alert("저장 완료!")
-})
 
-bookmark_fill.addEventListener('click',()=>{
-    bookmark_empty.style.display = 'block';
-    bookmark_fill.style.display = 'none';
-    alert("저장 취소 완료!")
-})
+bookmark_empty.addEventListener('click',(async function(){
+    const token = localStorage.getItem('access')
+    const result = await fetch(BASE_URL + '/bookmark_test/' + post_id,{
+        method:'POST',
+        mode: 'cors',
+        headers:{
+            'Authorization' : `Bearer ${token}`,
+            "Access-Control-Allow-Origin": "*",
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrftoken
+        },
 
-like_empty.addEventListener('click',()=>{
-    like_fill.style.display = 'block';
-    like_empty.style.display = 'none';
-    alert("좋아요 완료!")
-})
+    })
+    let res = await result.json()
+        if (result.ok){
+            alert("저장 완료 되었습니다!")
+            bookmark_fill.style.display = 'block';
+            bookmark_empty.style.display = 'none';
+        }   
+        else{
+            alert(res['messge'])
+        }
+    }))
 
-like_fill.addEventListener('click',()=>{
-    like_empty.style.display = 'block';
-    like_fill.style.display = 'none';
-    alert("좋아요 취소 완료!")
-})
+
+bookmark_fill.addEventListener('click',(async function(){
+    const token = localStorage.getItem('access')
+    const result = await fetch(BASE_URL + '/bookmark_test/' + post_id,{
+        method:'DELETE',
+        mode: 'cors',
+        headers:{
+            'Authorization' : `Bearer ${token}`,
+            "Access-Control-Allow-Origin": "*",
+            'Accept': 'application/json',
+            'Content-Type': 'application/json',
+            'X-CSRFToken': csrftoken
+        },
+    })
+    let res = await result.json()
+        if (result.ok){
+            alert("삭제 완료 되었습니다!")
+            bookmark_fill.style.display = 'none';
+            bookmark_empty.style.display = 'block';
+        }   
+        else{
+            alert(res['messge'])
+        }
+}))
+
+
